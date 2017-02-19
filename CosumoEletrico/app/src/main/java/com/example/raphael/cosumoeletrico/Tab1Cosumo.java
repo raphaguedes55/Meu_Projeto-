907d32792b8bb8acc9976d1966a4c17124f30334
Raphael <raphaguedes55@hotmail.com>
2017-02-19 14:30:02 -0300

    first commit

---
+++ CosumoEletrico/app/src/main/java/com/example/raphael/cosumoeletrico/Tab1Cosumo.java
@@ -0,0 +1,21 @@
+package com.example.raphael.cosumoeletrico;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by raphael on 20/11/16.
+ */
+
+public class Tab1Cosumo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tabconsumo, container, false);
+
+        return rootView;
+    }
+}
---
+++ CosumoEletrico/app/src/main/java/com/example/raphael/cosumoeletrico/Tab3Consumo.java
@@ -0,0 +1,25 @@
+package com.example.raphael.cosumoeletrico;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by raphael on 20/11/16.
+ */
+
+public class Tab3Consumo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tabconsumo3, container, false);
+
+        return rootView;
+
+    }
+
+}
