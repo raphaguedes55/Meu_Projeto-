65c537bdd66f3748bed67be36927d8fe920234d3
Matheus Brito <matheusbritodasilva@gmail.com>
2017-02-28 21:59:41 -0300

    modificado telainicio

---
+++ Ihadream/app/src/main/java/com/powellapps/ihadream/InicioActivity.java
@@ -0,0 +1,18 @@
+package com.powellapps.ihadream;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by matheus on 28/02/17.
+ */
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_inicio);
+    }
+}
